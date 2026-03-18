fbb2eae0bfda8a369cafdcf745dbe0bdc48defd8
Taimoor Hassan Kiani <taimoorkiani94@gmail.com>
2026-03-18 20:15:27 +0500

    Remove feedback.cy.ts

--- cypress/e2e/feedback.cy.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="cypress" />
-
-describe('Giphy App Smoke Test', () => {
-    it('opens the live app successfully', () => {
-        cy.visit('/')
-        cy.url().should('include', 'giphy-app-nu.vercel.app')
-    })
-})
